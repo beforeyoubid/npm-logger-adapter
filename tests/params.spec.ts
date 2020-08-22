bd4704a743189e52733e13350aba7ff2f362b715
Chainat <chainat@beforeyoubid.com.au>
2020-08-22 16:58:50 +1000

    Wrap params in a function so it's easier for unit test

--- tests/params.spec.ts
+++ tests/params.spec.ts
@@ -14,9 +14,76 @@ describe('Log Params', () => {
     expect(status).toEqual(false);
   });
 
-  it('validate interface', () => {
-    const params = getLogParams();
+  it('is able to set default logdna setting', () => {
+    const simulatedEnv = {
+      STAGE: 'sample',
+      LOGDNA_KEY: '',
+      LOGDNA_SUPPRESS_FLUSH_ALL: 'true',
+    };
+    const params = getLogParams(simulatedEnv);
     expect(params.logLevel).toEqual('info');
-    expect(params.logDNAKey).toEqual('');
+    expect(params.logDNASuppressFlushAll).toEqual(true);
+    expect(params.functionName).toEqual('n/a');
+    expect(params.logDNAOptions.env).toEqual('sample');
+  });
+
+  describe('sendToRemote', () => {
+    it('should set sendToRemote to false when logdna_key is empty', () => {
+      const simulatedEnv = {
+        LOGDNA_KEY: '',
+      };
+      const params = getLogParams(simulatedEnv);
+      expect(params.sendToRemote).toEqual(false);
+    });
+
+    it('should set sendToRemote to true when logdna_key presents & logdna is enabled', () => {
+      const simulatedEnv = {
+        LOGDNA_KEY: 'some-random-key',
+        LOGDNA_ENABLED: 'true',
+      };
+      const params = getLogParams(simulatedEnv);
+      expect(params.sendToRemote).toEqual(true);
+    });
+  });
+
+  describe('sendToRemote', () => {
+    it('should set sendToRemote to false when logdna_key is not set', () => {
+      const simulatedEnv = {};
+      const params = getLogParams(simulatedEnv);
+      expect(params.sendToRemote).toEqual(false);
+    });
+
+    it('should set sendToRemote to false when logdna_key is empty', () => {
+      const simulatedEnv = {
+        LOGDNA_KEY: '',
+      };
+      const params = getLogParams(simulatedEnv);
+      expect(params.sendToRemote).toEqual(false);
+    });
+
+    it('should set sendToRemote to true when logdna_key presents & logdna is enabled', () => {
+      const simulatedEnv = {
+        LOGDNA_KEY: 'some-random-key',
+        LOGDNA_ENABLED: 'true',
+      };
+      const params = getLogParams(simulatedEnv);
+      expect(params.sendToRemote).toEqual(true);
+    });
+  });
+
+  describe('Validate logDNASuppressFlushAll', () => {
+    it('should set logDNASuppressFlushAll to false by default', () => {
+      const simulatedEnv = {};
+      const params = getLogParams(simulatedEnv);
+      expect(params.logDNASuppressFlushAll).toEqual(false);
+    });
+
+    it('should set logDNASuppressFlushAll to true when we supply env variable', () => {
+      const simulatedEnv = {
+        LOGDNA_SUPPRESS_FLUSH_ALL: 'true',
+      };
+      const params = getLogParams(simulatedEnv);
+      expect(params.logDNASuppressFlushAll).toEqual(true);
+    });
   });
 });
