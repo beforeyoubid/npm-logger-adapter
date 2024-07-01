45f3f98086156ddb792c3339ab530a1128cc04cd
Chainat <chainat@beforeyoubid.com.au>
2024-07-02 09:00:36 +1000

    PLT-7189: refactor the code to support multiple prefix by utilising closure

--- src/logger/__tests__/util.spec.ts
+++ src/logger/__tests__/util.spec.ts
@@ -40,4 +40,24 @@ describe('getLoggerObject', () => {
     testLoggerObject.debug('my log message');
     expect(spy).toHaveBeenCalledWith('"my log message"');
   });
+
+  describe('Handle multiple loggers', () => {
+    it('should be able to handle multiple loggers with correct prefix', () => {
+      const spy = jest.spyOn(logger, 'debug');
+      const testLoggerObject1 = getLoggerObject('[MY-PREFIX-1]');
+      testLoggerObject1.debug('my log message');
+      expect(spy).toHaveBeenCalledWith('[MY-PREFIX-1]: "my log message"');
+      spy.mockReset();
+
+      const spy2 = jest.spyOn(logger, 'info');
+      const testLoggerObject2 = getLoggerObject('[MY-PREFIX-2]');
+      testLoggerObject2.info('my log message');
+      expect(spy2).toHaveBeenCalledWith('[MY-PREFIX-2]: "my log message"');
+
+      // try again on debug
+      const spy3 = jest.spyOn(logger, 'debug');
+      testLoggerObject1.debug('another log on the original logger');
+      expect(spy3).toHaveBeenCalledWith('[MY-PREFIX-1]: "another log on the original logger"');
+    });
+  });
 });
--- src/logger/util.ts
+++ src/logger/util.ts
@@ -3,7 +3,6 @@ import { WrappedLogger } from '../types';
 type LogFunction = (...args: unknown[]) => void;
 
 const logFunctionsToWrap = ['debug', 'info', 'warn', 'error', 'xxxx'];
-const wrappedLogger = {} as Record<string, (...args: unknown[]) => void>;
 
 const toString = (args: unknown[]) => {
   return args.map(arg => JSON.stringify(arg)).join(' ');
@@ -15,7 +14,8 @@ const getPrefix = (prefix: string) => (prefix && `${prefix}: `) || '';
  * A utility command to return a logger with a prefix on log line.
  * @param prefix string
  */
-const getLoggerObject = (prefix = ''): WrappedLogger => {
+const getLoggerObject = (prefix = '') => {
+  const wrappedLogger = {} as WrappedLogger;
   for (let functionName of logFunctionsToWrap) {
     wrappedLogger[functionName] = (...args: unknown[]) =>
       (logger[functionName] as LogFunction)(`${getPrefix(prefix)}${toString(args)}`);
